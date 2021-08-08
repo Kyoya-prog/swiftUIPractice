9cadb31bbec3cfef7ce9c1ffeed77af846a11e1b
kyoya <kyoyaprog@gmail.com>
2021-08-08 17:19:05 +0900

    セクションごとの表示

--- swiftUIPractice/ContentView.swift
+++ swiftUIPractice/ContentView.swift
@@ -15,27 +15,48 @@ struct Player:Identifiable {
 
 struct ContentView: View {
     
-    let players:[Player] = [
-        .init(id:0,name: "Yuta Watanabe", image: Image("Yuta Watanabe")),
+    let players:[[Player]] = [
+       [ .init(id:0,name: "Yuta Watanabe", image: Image("Yuta Watanabe")),
         .init(id:1,name: "rui hachimura", image: Image("rui hachimura")),
-        .init(id:2,name: "Kevin Durant", image: Image("Kevin Durant"))
+        .init(id:2,name: "Kevin Durant", image: Image("Kevin Durant")),
+        .init(id:2,name: "Jayson Tatum", image: Image("Jayson Tatum")),
+        .init(id:2,name: "James Harden", image: Image("James Harden")) ] ,
+        
+        [ .init(id:0,name: "Yuta Watanabe", image: Image("Yuta Watanabe")),
+         .init(id:1,name: "rui hachimura", image: Image("rui hachimura")),
+         .init(id:2,name: "Kevin Durant", image: Image("Kevin Durant")),
+         .init(id:2,name: "Jayson Tatum", image: Image("Jayson Tatum")),
+         .init(id:2,name: "James Harden", image: Image("James Harden")) ] ,
+        
+        [ .init(id:0,name: "Yuta Watanabe", image: Image("Yuta Watanabe")),
+         .init(id:1,name: "rui hachimura", image: Image("rui hachimura")),
+         .init(id:2,name: "Kevin Durant", image: Image("Kevin Durant")),
+         .init(id:2,name: "Jayson Tatum", image: Image("Jayson Tatum")),
+         .init(id:2,name: "James Harden", image: Image("James Harden")) ]
     ]
     
     var body: some View {
         NavigationView{
             List{
-                ForEach(players){ player in
-                    HStack{
-                        player.image
-                            .resizable()
-                            .scaledToFill()
-                            .clipShape(Circle())
-                            .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        Text(player.name)
-                            .font(.headline)
-                            .padding(.leading,20)
+                ForEach(players.startIndex ... (players.endIndex - 1),id:\.self){
+                    section in
+                    Section(header: "19902") {
+                        ForEach(self.players[section]){ player in
+                            HStack{
+                                player.image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .clipShape(Circle())
+                                    .frame(width: 50, height: 50)
+                                Text(player.name)
+                                    .font(.headline)
+                                    .padding(.leading,20)
+                            }
+                            
+                        }
                     }
-                }
+
+                    }
             }.navigationTitle(Text("NBA Player"))
         }
     }
