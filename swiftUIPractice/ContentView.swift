9ff1acd6a8c7aa0451948356bea6c629f182a96c
kyoya <kyoyaprog@gmail.com>
2021-08-08 17:01:53 +0900

    簡単なlistViewを実装

--- swiftUIPractice/ContentView.swift
+++ swiftUIPractice/ContentView.swift
@@ -7,10 +7,37 @@
 
 import SwiftUI
 
+struct Player:Identifiable {
+    var id:Int
+    let name: String
+    let image: Image
+}
+
 struct ContentView: View {
+    
+    let players:[Player] = [
+        .init(id:0,name: "Yuta Watanabe", image: Image("Yuta Watanabe")),
+        .init(id:1,name: "rui hachimura", image: Image("rui hachimura")),
+        .init(id:2,name: "Kevin Durant", image: Image("Kevin Durant"))
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                ForEach(players){ player in
+                    HStack{
+                        player.image
+                            .resizable()
+                            .scaledToFill()
+                            .clipShape(Circle())
+                            .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Text(player.name)
+                            .font(.headline)
+                            .padding(.leading,20)
+                    }
+                }
+            }.navigationTitle(Text("NBA Player"))
+        }
     }
 }
 
